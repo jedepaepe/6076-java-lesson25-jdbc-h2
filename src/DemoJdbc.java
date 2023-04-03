01d762425dcb0adc2d3cd08afd1c46bbf55183ef
Jean-Michel Depaepe <jean-michel.depaepe@equans.com>
2023-04-03 23:13:53 +0200

    more

---
+++ src/DemoJdbc.java
@@ -0,0 +1,41 @@
+import java.sql.*;
+
+public class DemoJdbc {
+    public static void main(String[] args) throws SQLException {
+        System.out.println("hello");
+
+        // connection
+        Connection connection = DriverManager.getConnection("jdbc:h2:./cats");
+        System.out.println("connected");
+
+        // créer la table
+        String sql = "create table if not exists CAT (ID integer primary key auto_increment, BREED varchar(32), SIZE double, WEIGHT double)";
+        Statement statement = connection.createStatement();
+        statement.execute(sql);
+
+        // insérer un record
+
+        sql = "insert into CAT (BREED, SIZE, WEIGHT) VALUES (?,?,?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setString(1, "Siamois");
+        preparedStatement.setDouble(2, 29.3);
+        preparedStatement.setDouble(3, 4.76);
+        preparedStatement.execute();
+
+        sql = "select * from CAT";
+        statement.getResultSet();
+        boolean isResultSet = statement.execute(sql);
+        System.out.println(isResultSet);
+        ResultSet resultSet = statement.getResultSet();
+        while (resultSet.next()) {
+            int id = resultSet.getInt("ID");
+            String breed = resultSet.getString("BREED");
+            double size = resultSet.getDouble("SIZE");
+            double weight = resultSet.getDouble("WEIGHT");
+            System.out.println(id + " - " + breed + " - " + size + " - " + weight);
+        }
+
+        // fermeture
+        connection.close();
+    }
+}
